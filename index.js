ad2e2acf955de75c5d9194c6516687fb083ec761
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 18:00:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function findMatching(someArray, name) {
 
 
 function fuzzyMatch(someArray, str){
-  return someArray.fitler(function() {
-    
+  return someArray.fitler(function(someStr) {
+    return someStr[0] == str[0];
   })
 }
