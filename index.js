96f7c178070c6168db57921ab690ad53e5665933
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 17:49:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(name, someArray) {
-  const result = someArray.filter(function (someName){
+  return someArray.filter(function (someName){
 
   }
   return result
