9e24c30c80238faf317744f103f7b8d9d1323e4a
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 17:53:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(someArray, name) {
   return someArray.filter(function (someName){
-    return someName.lowerCase() == name.lowerCase();
+    return someName.toLowerCase() == name.lowerCase();
   })
 }
