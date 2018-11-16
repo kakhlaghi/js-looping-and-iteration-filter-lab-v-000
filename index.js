ed14eb7fdd8451c521be74046c4d3fea4c632f76
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 17:53:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(someArray, name) {
   return someArray.filter(function (someName){
-    return someName == name;
+    return someName.lowerCase() == name.lowerCase();
   })
 }
