34c3d8facbcf492bff0243272840463259b7e323
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-16 17:55:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Code your solution in this file
 function findMatching(someArray, name) {
   return someArray.filter(function (someName){
-    return someName.toLowerCase() == name.lowerCase();
+    return someName.toLowerCase() == name.toLowerCase();
   })
 }
+
+
+function fuzzyMatch(someArray, str)
